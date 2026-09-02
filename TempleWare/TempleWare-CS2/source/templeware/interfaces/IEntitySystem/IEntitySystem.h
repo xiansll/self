bcad524bf2e4786492256d7fc4d5701765d0d175
xiansll <143344286+xiansll@users.noreply.github.com>
2026-09-01 21:42:29 -0400

    phase3a: harden entity-system local accessors

--- TempleWare/TempleWare-CS2/source/templeware/interfaces/IEntitySystem/IEntitySystem.h
+++ TempleWare/TempleWare-CS2/source/templeware/interfaces/IEntitySystem/IEntitySystem.h
@@ -18,19 +18,17 @@ public:
 	}
 
 	C_CSPlayerPawn* get_local_pawn() {
-
-		static auto fn = reinterpret_cast<C_CSPlayerPawn * (__fastcall*)(int)>(M::scan_absolute("client.dll", "E8 ? ? ? ? 48 8B F0 48 85 C0 74 ? 48 8D 15 ? ? ? ? B9", 0x1)); // x-ref to "Deathcam.Review_Start"; last call, struct call->mov->test->jz
-		return fn(0);
+		static auto fn = reinterpret_cast<C_CSPlayerPawn * (__fastcall*)(int)>(
+			M::scan_absolute("client.dll", "E8 ? ? ? ? 48 8B F0 48 85 C0 74 ? 48 8D 15 ? ? ? ? B9", 0x1)
+		); // x-ref to "Deathcam.Review_Start"; last call, struct call->mov->test->jz
+		return fn ? fn(0) : nullptr;
 	}
 
 	void* get_local_controller() {
-
 		static auto fn = reinterpret_cast<void* (__fastcall*)(int)>(
 			M::patternScan("client", "48 83 EC ? 83 F9 ? 75 ? 48 8B 0D ? ? ? ? 48 8D 54 24 ? 48 8B 01 FF 90 ? ? ? ? 8B 08 48 63 C1 48 8D 0D ? ? ? ? 48 8B 04 C1 48 83 C4 ? C3 CC CC CC CC CC CC CC CC CC CC CC CC CC 48 83 EC ? 83 F9")
 			);
-		if (fn)
-			return fn(-1);
-		return nullptr;
+		return fn ? fn(-1) : nullptr;
 	}
 
 	int get_highest_entity_index() {
